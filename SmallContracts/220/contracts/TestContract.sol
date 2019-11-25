5b6326de7db2827a43db2e5bd9a50aeb601cb597
Stefan Willem Driessen <swdriessen@gmail.com>
2019-11-25 11:37:55 +0100

    prepping for the second round of tests

---
+++ SmallContracts/220/contracts/TestContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract TestContract{
+
+constructor() public {
+}
+
+function TestOne(uint a, uint b) public view returns(bool success){
+  return true;
+}
+
+function TestTwo(uint c, uint d) public view returns(bool success){
+  return true;
+}
+
+}
