ce9ea1b8fae35d4eec06e76f1ad719a9da9d2408
Stefan Willem Driessen <swdriessen@gmail.com>
2019-12-06 13:31:13 +0100

    Added bigger contracts

---
+++ Smart Contracts/leq60301/contracts/TestContract.sol
@@ -0,0 +1,277 @@
+pragma solidity ^0.5.0;
+
+contract TestContract{
+
+constructor() public {
+}
+
+function TestOne(uint a) public view returns(bool success){
+  if (a < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwo(uint b) public view returns(bool success){
+  if (b < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestThree(uint c) public view returns(bool success){
+  if (c < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestFour(uint d) public view returns(bool success){
+  if (d < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestFive(uint e) public view returns(bool success){
+  if (e < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestSix(uint f) public view returns(bool success){
+  if (f < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestSeven(uint g) public view returns(bool success){
+  if (g < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestEight(uint h) public view returns(bool success){
+  if (h < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestNine(uint i) public view returns(bool success){
+  if (i < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTen(uint j) public view returns(bool success){
+  if (j < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestEleven(uint k) public view returns(bool success){
+  if (k < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwelve(uint l) public view returns(bool success){
+  if (l < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestThirteen(uint m) public view returns(bool success){
+  if (m < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestFourteen(uint n) public view returns(bool success){
+  if (n < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestFifteen(uint o) public view returns(bool success){
+  if (o < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestSixteen(uint p) public view returns(bool success){
+  if (p < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestSeventeen(uint q) public view returns(bool success){
+  if (q < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestEighteen(uint r) public view returns(bool success){
+  if (r < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestNineteen(uint s) public view returns(bool success){
+  if (s < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwenty(uint t) public view returns(bool success){
+  if (t < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyOne(uint u) public view returns(bool success){
+  if (u < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyTwo(uint v) public view returns(bool success){
+  if (v < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyThree(uint w) public view returns(bool success){
+  if (w < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyFour(uint x) public view returns(bool success){
+  if (x < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyFive(uint o) public view returns(bool success){
+  if (o < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentySix(uint p) public view returns(bool success){
+  if (p < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentySeven(uint q) public view returns(bool success){
+  if (q < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyEight(uint r) public view returns(bool success){
+  if (r < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestTwentyNine(uint s) public view returns(bool success){
+  if (s < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+
+function TestThirty(uint t) public view returns(bool success){
+  if (t < 13){
+    return false;
+  }
+  else{
+      return true;
+  }
+}
+}
